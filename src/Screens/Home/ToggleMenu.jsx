5cc92c51c28cf93bb8f955b296ded305e5acf661
Mohammad Mujeeb Ur Rahman <98672856+MujeebMudassir@users.noreply.github.com>
2026-02-26 16:01:16 +0530

    Update ToggleMenu.jsx

--- src/Screens/Home/ToggleMenu.jsx
+++ src/Screens/Home/ToggleMenu.jsx
@@ -29,7 +29,7 @@ const ToggleMenu = () => {
         <div className="p-4 pt-20">
           <div className="flex flex-col">
             <Link
-              to={"/pathsetter"}
+              to={"/"}
               onClick={() => {
                 scrollTo(0, 0);
                 setIsOpen(false);
